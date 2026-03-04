df4941458c8f58eb712f3e5350375d9f11b896ae
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-03-04 08:57:10 -0600

    added robots.txt and sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://creeksidemarketingpros.com',
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     port: 3000,
     open: true, // Set to true if you want the browser to open on `npm run dev`
   },
+
+  integrations: [sitemap()],
 });
